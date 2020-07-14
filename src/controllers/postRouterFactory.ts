1cd367f728b4c184cfbc821a3cc45514553b6faa
Oliver Saternus <mail@oliversaternus.de>
2020-07-14 13:22:33 +0200

    refactor naming

--- src/controllers/postRouterFactory.ts
+++ src/controllers/postRouterFactory.ts
@@ -6,16 +6,16 @@ import { autoVerifyUser } from '../tools/auth';
 export const postRouterFactory = () => Router()
 
   .get('/posts', autoCatch(async (req, res, next) => {
-    const users = await Post.findAll();
-    res.json(users);
+    const posts = await Post.findAll();
+    res.json(posts);
   }))
 
   .get('/posts/:id', autoCatch(async (req, res, next) => {
-    const user = await Post.findByPk(req.params.id);
-    user ? res.json(user) : next({ statusCode: 404 });
+    const post = await Post.findByPk(req.params.id);
+    post ? res.json(post) : next({ statusCode: 404 });
   }))
 
   .post('/posts', autoCatch(autoVerifyUser(async (req, res, currentUser, next) => {
-    const user = await Post.create({ ...req.body, userId: currentUser.id });
-    res.json(user);
+    const post = await Post.create({ ...req.body, userId: currentUser.id });
+    res.json(post);
   })));
